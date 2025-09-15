5e0c4579eaf05a1a50a35adb59cc5920dc4846be
Shashank Poojary <138311524+shashankpooj@users.noreply.github.com>
2025-09-15 23:46:00 +0530

    Update Targetmodel.py

--- backend/Targetmodel.py
+++ backend/Targetmodel.py
@@ -8,8 +8,8 @@ from torch_geometric.nn import global_mean_pool
 class TargetModel(nn.Module):
    def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        x = F.relu(self.conv1(x, edge_index))
-        x = F.relu(self.conv2(x, edge_index))
+        x = F.relu(self.GCNconv1(x, edge_index))
+        x = F.relu(self.GCNconv2(x, edge_index))
         x = global_mean_pool(x, data.batch)  # pool node features to graph features
         x = self.fc(x)
         return x
@@ -18,8 +18,8 @@ class TargetModel(nn.Module):
 class HitModel(nn.Module):
       def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        x = F.relu(self.conv1(x, edge_index))
-        x = F.relu(self.conv2(x, edge_index))
+        x = F.relu(self.GCNconv1(x, edge_index))
+        x = F.relu(self.GCNconv2(x, edge_index))
         x = global_mean_pool(x, data.batch)  # pool node features to graph features
         x = self.fc(x)
         return x
@@ -27,8 +27,8 @@ class HitModel(nn.Module):
 class ADMETModel(nn.Module):
        def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        x = F.relu(self.conv1(x, edge_index))
-        x = F.relu(self.conv2(x, edge_index))
+        x = F.relu(self.GCNconv1(x, edge_index))
+        x = F.relu(self.GCNconv2(x, edge_index))
         x = global_mean_pool(x, data.batch)  # pool node features to graph features
         x = self.fc(x)
         return x
