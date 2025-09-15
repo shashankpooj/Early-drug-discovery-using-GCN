bd03bfe4c1a75429ffbee0bc14ec29baa6b4fa61
Shashank Poojary <138311524+shashankpooj@users.noreply.github.com>
2025-09-15 23:54:11 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -35,28 +35,7 @@ kmer_vocab_file = '/Users/shashank/Desktop/Early_ddrug/data/kmer_vocab.txt'
 try:
     target_model = load_model(TargetModel, "/Users/shashank/Desktop/Early_ddrug/data/saved_models/target_model.pth", input_dim=input_dim_target, hidden_dim=hidden_dim_target)
     hit_model = load_model(HitModel, "/Users/shashank/Desktop/Early_ddrug/data/saved_models/hit_model.pth", input_dim=input_dim_hit, hidden_dim=hidden_dim_hit)
-    admet_model_dir = "/Users/shashank/Desktop/Early_ddrug/data/saved_models/ADMETpredict"
-    admet_model = dc.deepchem.models.GraphConvModel(
-        n_tasks=12,
-        mode='classification',
-        batch_normalize=False  # Match this to the saved model's configuration
-    )
-    # Restore the model from the saved checkpoint
-    admet_model.restore(model_dir=admet_model_dir)
-except Exception as e:
-    print(f"Error loading models: {e}")
-
-LEAD_data_path = '/Users/shashank/Desktop/Early_ddrug/data/250k_rndm_zinc_drugs_clean_3.csv'
-def load_dataset_LEAD(file_path):
-    df = pd.read_csv(file_path)
-    smiles_list = df['smiles']  # Replace 'smiles' with the column name containing SMILES strings
-    return [Chem.MolFromSmiles(smiles) for smiles in smiles_list if Chem.MolFromSmiles(smiles) is not None]
     
-def load_tasks(filename="tasks.pkl"):
-  with open(filename, "rb") as f:
-    return pickle.load(f)
-
-tasks = load_tasks('/Users/shashank/Desktop/Early_ddrug/data/tasks.pkl')
 
 # Target identification Phase:
 def kmer_count(sequence, k=3):
